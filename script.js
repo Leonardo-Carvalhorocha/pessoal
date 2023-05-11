814e5725ac2d33bcd91edd35336faf9211190193
Leonardo Carvalho <leonardo2001darocha@gmail.com>
2023-05-10 22:22:51 -0300

    scroll suave

--- script.js
+++ script.js
@@ -20,3 +20,24 @@ function initAnimacaoScroll() {
     }
 }
 initAnimacaoScroll();
+
+
+function initScroll() {
+  const linkInterno = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollSuave(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+  })
+  }
+
+  linkInterno.forEach((link) => {
+    link.addEventListener('click', scrollSuave);
+  })
+}
+initScroll()
+
